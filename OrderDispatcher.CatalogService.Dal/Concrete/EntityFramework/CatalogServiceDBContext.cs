2a580e9e0c2277ba496935724c36de553433ed1f
esats <esat.avsr@gmail.com>
2026-03-16 02:44:40 -0400

    uo

--- OrderDispatcher.CatalogService.Dal/Concrete/EntityFramework/CatalogServiceDBContext.cs
+++ OrderDispatcher.CatalogService.Dal/Concrete/EntityFramework/CatalogServiceDBContext.cs
@@ -20,15 +20,6 @@ namespace OrderDispatcher.CatalogService.Dal.Concrete.EntityFramework
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-
-
-            if (!optionsBuilder.IsConfigured)
-            {
-
-                optionsBuilder.UseSqlServer(@"Server=.;database=OrderDP.CatalogDB;Trusted_Connection=True;MultipleActiveResultSets=true;Connect Timeout=150;TrustServerCertificate=True");
-                //optionsBuilder.UseSqlServer(@"server=217.195.207.190;Initial Catalog=TexEx;User ID=Texexadmin;password=Texex2121**!!!");
-            }
-
             optionsBuilder.EnableSensitiveDataLogging();
             optionsBuilder.EnableDetailedErrors();
         }
