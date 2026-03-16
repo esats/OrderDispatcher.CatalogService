2d7ec16499cad33c0ca4ad35870028feebf08fe4
esats <esat.avsr@gmail.com>
2026-03-16 02:59:27 -0400

    trial

--- OrderDispatcher.CatalogService.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ OrderDispatcher.CatalogService.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -20,64 +20,44 @@ namespace OrderDispatcher.CatalogService.Core.EntityFramework
             _context = context;
         }
 
-        protected EfEntityRepositoryBase() { }
-
         public TEntity Add(TEntity entity)
         {
-            using (var context = new TContext())
-            {
-                var addedEntity = context.Entry(entity);
-                addedEntity.State = EntityState.Added;
-                context.SaveChanges();
-                return entity;
-            }
+            var addedEntity = _context.Entry(entity);
+            addedEntity.State = EntityState.Added;
+            _context.SaveChanges();
+            return entity;
         }
 
         public async Task<TEntity> AddAsync(TEntity entity)
         {
-            using (var context = new TContext())
-            {
-                var addedEntity = context.Entry(entity);
-                addedEntity.State = EntityState.Added;
-                await context.SaveChangesAsync();
-                return entity;
-            }
+            var addedEntity = _context.Entry(entity);
+            addedEntity.State = EntityState.Added;
+            await _context.SaveChangesAsync();
+            return entity;
         }
 
         public void Delete(TEntity entity)
         {
-            using (var context = new TContext())
-            {
-                var deletedEntity = context.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-            }
+            var deletedEntity = _context.Entry(entity);
+            deletedEntity.State = EntityState.Deleted;
+            _context.SaveChanges();
         }
 
         public async Task DeleteAsync(TEntity entity)
         {
-            using (var context = new TContext())
-            {
-                var deletedEntity = context.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                await context.SaveChangesAsync();
-            }
+            var deletedEntity = _context.Entry(entity);
+            deletedEntity.State = EntityState.Deleted;
+            await _context.SaveChangesAsync();
         }
-
 
         public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
         {
-            using (var context = new TContext())
-            {
-                return filter == null ? context.Set<TEntity>().ToList() : context.Set<TEntity>().Where(filter).ToList();
-            }
+            return filter == null ? _context.Set<TEntity>().ToList() : _context.Set<TEntity>().Where(filter).ToList();
         }
 
         public async Task<List<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> filter = null)
         {
-            await using var context = new TContext();
-
-            var query = context.Set<TEntity>().AsQueryable();
+            var query = _context.Set<TEntity>().AsQueryable();
 
             if (filter != null)
             {
@@ -89,42 +69,28 @@ namespace OrderDispatcher.CatalogService.Core.EntityFramework
 
         public TEntity GetT(Expression<Func<TEntity, bool>> filter = null)
         {
-            using (var context = new TContext())
-            {
-                return context.Set<TEntity>().SingleOrDefault(filter);
-            }
+            return _context.Set<TEntity>().SingleOrDefault(filter);
         }
 
         public async Task<TEntity> GetTAsync(Expression<Func<TEntity, bool>> filter = null)
         {
-            using (var context = new TContext())
-            {
-                return await context.Set<TEntity>().SingleOrDefaultAsync(filter);
-            }
+            return await _context.Set<TEntity>().SingleOrDefaultAsync(filter);
         }
 
         public TEntity Update(TEntity entity)
         {
-            using (var context = new TContext())
-            {
-                var UpdatetedEntity = context.Entry(entity);
-                //var UpdatetedEntity = context.Update(entity);
-                
-                UpdatetedEntity.State = EntityState.Modified;
-                context.SaveChanges();
-                return entity;
-            }
+            var UpdatetedEntity = _context.Entry(entity);
+            UpdatetedEntity.State = EntityState.Modified;
+            _context.SaveChanges();
+            return entity;
         }
 
         public async Task<TEntity> UpdateAsync(TEntity entity)
         {
-            using (var context = new TContext())
-            {
-                var UpdatetedEntity = context.Entry(entity);
-                UpdatetedEntity.State = EntityState.Modified;
-                await context.SaveChangesAsync();
-                return entity;
-            }
+            var UpdatetedEntity = _context.Entry(entity);
+            UpdatetedEntity.State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return entity;
         }
     }
 }
